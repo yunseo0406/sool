223618dd82ce889489b9c0f650e7d83151116e2c
yunseo0406 <lcss0406@gmail.com>
2022-10-06 23:01:13 +0900

    first commit

---
+++ sool.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+	int glass;
+
+	printf("정연이의 주량은? (몇잔, 8잔 이하)");
+
+	scanf("%d", &glass);
+
+ 
+
+	(glass > 5) ? printf("그렇게 잘마시지 않습니다.") : 
+
+		(glass <= 5 && glass > 3) ? printf("좀 더 못마십니다.") : printf("ㅋㅋ 정답이요");
+
+ 
+
+	return 0;
+
+}
